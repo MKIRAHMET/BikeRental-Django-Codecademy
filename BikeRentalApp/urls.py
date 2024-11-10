d32238a1a491227c9c967f0d10db37f6b8e2ae2b
M KIR AHMET <mkirahmet@gmail.com>
2024-11-10 17:21:31 +0200

    urls and base of views done. Need to create template to fetch data from Database and add context to views

--- BikeRentalApp/urls.py
+++ BikeRentalApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
    path("", views.home, name="home"),
+   path("bikelist", views.bikelist, name="bikelist"),
+   path("renterlist", views.renterlist, name="renterlist"),
+   path("rentallist", views.rentallist, name="rentallist")
 ]
--- BikeRentalApp/views.py
+++ BikeRentalApp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'BikeRentalApp/home.html')
+
+def bikelist(request):
+   return render(request, 'BikeRentalApp/home.html')
+
+def renterlist(request):
+   return render(request, 'BikeRentalApp/home.html')
+
+def rentallist(request):
+   return render(request, 'BikeRentalApp/home.html')
