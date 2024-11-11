de3b0f40c1073466b037c03f2bc7bebdb567103a
M KIR AHMET <mkirahmet@gmail.com>
2024-11-11 16:04:47 +0200

    Bike List ready

--- BikeRentalApp/urls.py
+++ BikeRentalApp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
    path("", views.home, name="home"),
-   path("bikelist", views.bikelist, name="bikelist"),
+   path("bikeList", views.bikeList.as_view(), name="bikeList"),
    path("renterlist", views.renterlist, name="renterlist"),
    path("rentallist", views.rentallist, name="rentallist")
 ]
--- BikeRentalApp/views.py
+++ BikeRentalApp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Bike, Renter, Rental
+from django.views.generic import ListView
 
 # Create your views here.
 def home(request):
     return render(request, 'BikeRentalApp/home.html')
 
-def bikelist(request):
-   return render(request, 'BikeRentalApp/home.html')
+class bikeList(ListView):
+  model = Bike
+  template_name = "BikeRentalApp/bikelist.html"
 
 def renterlist(request):
-   return render(request, 'BikeRentalApp/home.html')
+   return render(request, 'BikeRentalApp/renterlist.html')
 
 def rentallist(request):
-   return render(request, 'BikeRentalApp/home.html')
+   return render(request, 'BikeRentalApp/rentallist.html')
